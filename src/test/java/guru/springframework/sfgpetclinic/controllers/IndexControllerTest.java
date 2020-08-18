6b2ff02e39bc16d50008091b2364015ca8daae17
vesem <mitchellthom@gmail.com>
2020-08-17 20:30:55 -0400

    Timeout example

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,8 +1,11 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,6 +30,23 @@ class IndexControllerTest {
     void oupsHandler() {
         assertThrows(ValueNotFoundException.class, () -> {
             controller.oupsHandler();
+        });
+    }
+
+    @Disabled("Demo of timeout")
+    @Test
+    void testTimeOut() {
+        assertTimeout(Duration.ofMillis(100), () -> {
+            Thread.sleep(2000);
+        });
+    }
+
+    @Disabled("Demo of timeout")
+    @Test
+    void testTimeOutPrempt() {
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            Thread.sleep(2000);
+            System.out.println("I got here");
         });
     }
 }
